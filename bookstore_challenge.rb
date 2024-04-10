b41738ce3f67c1a363b33f3c387cc785c15df25e
raonircl <raoni.afi@gmailcom>
2024-04-10 13:48:48 -0300

    feat: desafio livraria

---
+++ bookstore_challenge.rb
@@ -0,0 +1,8 @@
+print "Digite seu nome: "
+name = gets.chomp
+print "Digite seu sobrenome: "
+lastname = gets.chomp
+print "Digite sua idade: "
+age = gets.chomp
+
+puts "Seja bem-vindo, #{name} #{lastname}, #{age}"
